7634763b0f42acc99e7f183c5c74cb93fe156b76
johnneshagraves <johnneshagraves@gmail.com>
2025-12-24 15:56:05 -0500

    updated components

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,33 +3,67 @@ import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
-  let [results, setResults] = useState({});
+export default function Dictionary(props) {
+  const [keyword, setKeyword] = useState(props.defaultKeyword);
+  const [loaded, setLoaded] = useState(false);
+  const [definition, setDefinition] = useState(null);
+  const [photos, setPhotos] = useState([]);
 
-  function handleResponse(response) {
-    setResults(response.data[0]);
+  function handleImages(response) {
+    setPhotos(response.data.photos);
   }
 
-  function search(event) {
-    event.preventDefault();
+  function handleResponse(response) {
+    setDefinition(response.data);
+    let apiKey = "a723fd412o41a9d1a23tfcb7443f0307";
+    let apiUrl = `https://api.shecodes.io/images/v1/search?query=${response.data.word}&key=${apiKey}`;
+    axios
+      .get(apiUrl, { headers: { Authorization: `Bearer ${apiKey}` } })
+      .then(handleImages);
+  }
 
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  function search() {
     let apiKey = "a723fd412o41a9d1a23tfcb7443f0307";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <label>Enter a word</label>
+            <input
+              type="search"
+              placeholder="Search for a word"
+              defaultValue={props.defaultKeyword}
+              autoFocus={true}
+              className="form-control search-input"
+              onChange={handleKeywordChange}
+            />
+          </form>
+          <small className="hint">i.e. hot, wine, sunset, coding</small>
+        </section>
+        <Results definition={definition} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading!";
+  }
 }
