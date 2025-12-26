475fc41756902d351504e2ee859c762928ac6779
johnneshagraves <johnneshagraves@gmail.com>
2025-12-25 21:40:25 -0500

    changed h3 to h4

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,7 +4,7 @@ export default function Meaning(props) {
   if (props.meaning) {
     return (
       <div className="Meaning">
-        <h3>{props.meaning.partOfSpeech}</h3>
+        <h4>{props.meaning.partOfSpeech}</h4>
         <p>{props.meaning.definition}</p>
         <em>{props.meaning.example}</em>
       </div>
