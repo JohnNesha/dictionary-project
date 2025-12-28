eb68fe9c2f77cf6f605f93d0677d26c9a51540e0
johnneshagraves <johnneshagraves@gmail.com>
2025-12-28 14:48:41 -0500

    removed undefined

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,9 +4,8 @@ import Result from "./Results";
 import Photos from "./Photos";
 import "./Dictionary.css";
 
-export default function Dictionary(props) {
-  const [keyword, setKeyword] = useState(props.defaultKeyword);
-  const [loaded, setLoaded] = useState(false);
+export default function Dictionary() {
+  const [keyword, setKeyword] = useState("");
   const [definition, setDefinition] = useState(null);
   const [photos, setPhotos] = useState([]);
 
@@ -16,6 +15,7 @@ export default function Dictionary(props) {
 
   function handleResponse(response) {
     setDefinition(response.data);
+
     let apiKey = "a723fd412o41a9d1a23tfcb7443f0307";
     let apiUrl = `https://api.shecodes.io/images/v1/search?query=${response.data.word}&key=${apiKey}`;
     axios
@@ -23,12 +23,9 @@ export default function Dictionary(props) {
       .then(handleImages);
   }
 
-  function load() {
-    setLoaded(true);
-    search();
-  }
-
   function search() {
+    if (!keyword) return;
+
     let apiKey = "a723fd412o41a9d1a23tfcb7443f0307";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
@@ -43,29 +40,25 @@ export default function Dictionary(props) {
     setKeyword(event.target.value);
   }
 
-  if (loaded) {
-    return (
-      <div className="Dictionary">
-        <section>
-          <form onSubmit={handleSubmit}>
-            <label>What word do you want to look up?</label>
-            <input
-              type="search"
-              placeholder="Search for a word"
-              defaultValue={props.defaultKeyword}
-              autoFocus={true}
-              className="form-control search-input"
-              onChange={handleKeywordChange}
-            />
-          </form>
-          <small className="hint">i.e. paris, wine, yoga, coding</small>
-        </section>
-        <Result definition={definition} />
-        <Photos photos={photos} />
-      </div>
-    );
-  } else {
-    load();
-    return "Loading!";
-  }
+  return (
+    <div className="Dictionary">
+      <section>
+        <form onSubmit={handleSubmit}>
+          <label>What word do you want to look up?</label>
+          <input
+            type="search"
+            placeholder="Search for a word"
+            value={keyword}
+            onChange={handleKeywordChange}
+            autoFocus={true}
+            className="form-control search-input"
+          />
+        </form>
+        <small className="hint">i.e. paris, wine, yoga, coding</small>
+      </section>
+
+      {definition && <Result definition={definition} />}
+      {photos.length > 0 && <Photos photos={photos} />}
+    </div>
+  );
 }
